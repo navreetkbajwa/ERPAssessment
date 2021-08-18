833d958b143c45e3bd2b0f28efa1a80e422a49cc
navreetkbajwa <navreetkbajwa@gmail.com>
2021-08-18 11:48:00 -0400

    updating tests, fixing git issues

--- src/SieveOfEratosthenesTest.java
+++ src/SieveOfEratosthenesTest.java
@@ -7,6 +7,7 @@ class SieveOfEratosthenesTest {
 
     @org.junit.jupiter.api.Test
     void test1() {
+        //should fail
         ArrayList<String> test1 = new ArrayList<>();
         test1.add("2");
         test1.add("3");
@@ -16,6 +17,7 @@ class SieveOfEratosthenesTest {
 
     @org.junit.jupiter.api.Test
     void test2() {
+        //should pass
         ArrayList<String> test2 = new ArrayList<>();
         test2.add("2");
         test2.add("3");
@@ -32,6 +34,7 @@ class SieveOfEratosthenesTest {
 
     @org.junit.jupiter.api.Test
     void test3() {
+        //should pass
         ArrayList<String> test3 = new ArrayList<>();
         test3.add("2");
         test3.add("3");
@@ -42,6 +45,7 @@ class SieveOfEratosthenesTest {
 
     @org.junit.jupiter.api.Test
     void test4() {
+        //should fail
         ArrayList<String> test4 = new ArrayList<>();
         test4.add("2");
         test4.add("3");
@@ -52,6 +56,7 @@ class SieveOfEratosthenesTest {
 
     @org.junit.jupiter.api.Test
     void test5() {
+        //edge case, should pass
         ArrayList<String> test5 = new ArrayList<>();
         Assertions.assertEquals(SieveOfEratosthenes.sieveOfEratosthenes(0),test5);
     }
